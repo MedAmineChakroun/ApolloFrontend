68f6cacc0a5ab5ae3e603b111944d83c37d03348
Fedi Boughnimi <fediboughnimi2@gmail.com>
2025-05-07 22:32:20 +0200

    add edit order functionality with validation and update methods

--- src/app/features/customer/customer.routes.ts
+++ src/app/features/customer/customer.routes.ts
@@ -3,13 +3,16 @@ import { Landing } from '../landing/landing';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
+import { EditOrderComponent } from './edit-order/edit-order.component';
 import { RoleGuard } from '../../core/guards/role.guard';
 import { AuthGuard } from '../../core/guards/auth.guard';
 export default [
     { path: '', component: Landing },
     { path: 'profile', component: UserProfileComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['customer', 'admin'] } },
     { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['customer'] } },
-    { path: 'orderDetails/:id', component: OrderDetailsComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['customer', 'admin'] } }
+    { path: 'orderDetails/:id', component: OrderDetailsComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['customer', 'admin'] } },
+    { path: 'orders/edit/:id', component: EditOrderComponent , canActivate: [AuthGuard, RoleGuard], data: { roles: ['customer', 'admin'] } }
+    
     // { path: 'product/:id', component: ProductComponent },
     // { path: 'cart', component: CartComponent },
 ] as Routes;
